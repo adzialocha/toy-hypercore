b3690e006502e79c601f16997e77c50be40f4235
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-02-19 20:57:29 +0100

    Add basic event loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate base64;
 extern crate blake2_rfc;
 extern crate ed25519_dalek;
+extern crate futures;
 extern crate getopts;
 extern crate hex;
 extern crate rand;
@@ -8,7 +9,17 @@ extern crate sha2;
 
 pub mod crypto;
 
+use futures::{Future, Async};
+use tokio_core::reactor::{Core, Handle};
+
 const DAT_URL_PROTOCOL: &str = "dat://";
+
+fn run(handle: Handle, discovery_key_full: &[u8], token: String)
+    -> impl Future<Item=(), Error=()>
+{
+    // Never end this future
+    futures::future::poll_fn(|| Ok(Async::NotReady))
+}
 
 fn main() {
     // Parse command-line arguments
@@ -39,11 +50,19 @@ fn main() {
 
     println!("{}{}", DAT_URL_PROTOCOL, hex::encode(public_key));
 
-    // Build a discovery key (hashed public key and name)
+    // Build discovery key (hashed public key and name)
     let discovery_key = crypto::generate_discovery_key(public_key);
 
-    // Generate an individual token to identify ourselves
+    // Generate individual token to identify ourselves
     let token = crypto::generate_random_token();
 
-    println!("Discovery Key: {:?}\nToken: {}", discovery_key.as_bytes(), token);
+    // Create event loop to drive the networking I/O
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+
+    // Start main task
+    let main = run(handle.clone(), discovery_key.as_bytes(), token);
+
+    // ... and add it to event loop
+    core.run(main).unwrap();
 }
